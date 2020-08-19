081cf2b389cb6291b93b1018fd080143aed73539
dikac <rigel234@yahoo.com>
2020-08-19 20:34:58 +0700

    dependency update & introduce t-code

--- dist/response/code/code.d.ts
+++ dist/response/code/code.d.ts
@@ -1,3 +1,6 @@
-export default interface Code<Code extends number = number> {
-    code: Code;
+import CodeInterface from "@dikac/t-code/code";
+/**
+ * @deprecated use {@link CodeInterface} instead
+ */
+export default interface Code<Code extends number = number> extends CodeInterface<Code> {
 }
--- src/response/code/code.ts
+++ src/response/code/code.ts
@@ -1,5 +1,10 @@
+import CodeInterface from "@dikac/t-code/code";
+
+/**
+ * @deprecated use {@link CodeInterface} instead
+ */
 export default interface Code<
-    Code extends number = number,
-> {
-    code : Code;
+    Code extends number = number
+> extends CodeInterface<Code> {
+
 }
